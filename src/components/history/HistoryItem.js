0353377a6ab88880e59546ee9d4126d2e77ffe87
Ilya <xatyt@mail.ru>
2020-08-31 20:20:57 +0300

    First Commit

---
+++ src/components/history/HistoryItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HistoryItem = ({ transaction, dellTransaction }) => (
+  <li className={`history__item history__item-${transaction.add ? 
+    'plus' : 
+    'minus'}`}>
+      {transaction.description}
+    <span className="history__money">{transaction.amount}</span>
+    <button 
+      className="history__delete" 
+      onClick={() => dellTransaction(transaction.id)}
+      >x</button>
+  </li>  
+);
+
+export default HistoryItem;
